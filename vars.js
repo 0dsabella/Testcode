7fae03228f7c03c7df47226a19f83da2d8134f5a
0dsabella <42749703+0dsabella@users.noreply.github.com>
2018-08-30 13:55:55 -0500

    Add files via upload

---
+++ vars.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = -25;
+let c = 25;
+let d = -10;
+//create two more variables c and d and give them whatever number you want.
+
+
+const total = a+b+c+d;
+console.log(total);
+
+
+
+//new section on string variables
+let convo = "hello ";
+//set name to your name, first and last.
+let name = "Dominic Sabella";
+//add string convo and name together.
+const greeting = convo + name;
+console.log(greeting);
+
+//make your own message similar to what is above.
+let convo2 = "greetings ";
+let convo3 = "human";
+const open = convo2 + convo3;
+console.log();
